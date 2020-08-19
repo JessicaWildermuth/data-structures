c4f3de93a35bee92b640e3383bfbae44a1c1e974
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-19 15:47:15 -0700

    Complete methods and tests pass

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,11 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var newQueue = {};
-  var storage = {};
-  var size = 0;
-  return _.extend(newQueue, Queue.queueMethods);
+  var queue = {};
+  queue.storage = {};
+  queue.sizeCount = 0;
+  return _.extend(queue, queueMethods);
 };
 
-// Queue.queueMethods = {};
+var queueMethods = {};
 
+queueMethods.size = function() {
+  var howMany = Object.keys(this.storage).length;
+  return howMany;
+};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.sizeCount] = value;
+  this.sizeCount++;
+};
+
+queueMethods.dequeue = function(value) {
+  var firstToLast = Object.keys(this.storage).sort();
+  var first = firstToLast.shift();
+  var result = this.storage[first];
+  delete this.storage[first];
+  return result;
+};
