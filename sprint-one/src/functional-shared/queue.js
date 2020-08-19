3e4a6bf09996eac0a96af1569c7a114f85779af6
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-19 15:32:04 -0700

    Add this keyword for correct method use

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,53 +3,9 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var newQueue = {};
   var storage = {};
-  var sie = 0;
+  var size = 0;
   return _.extend(newQueue, Queue.queueMethods);
 };
 
 // Queue.queueMethods = {};
 
-// queueMethods.enqueue = function(value) {
-//   this.storage[size] = value;
-//   this.value++;
-// };
-
-// queueMethods.dequeue = function(value) {
-//   var keys = Object.keys(storage).sort();
-//   var firstElement = key[0];
-//   delete this.storage[firstElement];
-//   return firstElement;
-// };
-
-// queueMethods.size = function() {
-//   var keyLength = Object.keys(storage).length;
-//   return keyLength;
-// };
-
-
-// var giraffeMaker = function(name, height) {
-//   var newGiraffe = {};
-//   newGiraffe.name = name;
-//   newGiraffe.height = height;
-//   newGiraffe.hunger = 10;
-//   extend(newGiraffe, giraffeMaker.giraffeMethods);
-
-//   return newGiraffe;
-// };
-
-// var extend = function(to, from) {
-//   for (var key in from) {
-//     to[key] = from[key];
-//   }
-// };
-
-// giraffeMaker.giraffeMethods = {};
-
-
-// giraffeMaker.giraffeMethods.isTallEnough = function(treeHeight) {
-//     return this.height > treeHeight;
-//   };
-
-// giraffeMaker.giraffeMethods.isHungry = function() {
-//   return this.hunger > 0;
-// };
