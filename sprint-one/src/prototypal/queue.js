374fc44425c9c82e8d18faaf9e86b46390897568
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-19 16:26:11 -0700

    Code complete - small change from function-shared to work

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.sizeCount = 0;
+  queue.storage = {};
+  return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function () {
+  var howMany = Object.keys(this.storage).length;
+  return howMany;
+};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.sizeCount] = value;
+  this.sizeCount++;
+};
+
+queueMethods.dequeue = function(value) {
+  var firstToLast = Object.keys(this.storage).sort();
+  var first = firstToLast.shift();
+  var result = this.storage[first];
+  delete this.storage[first];
+  return result;
+};
