9435747b06d70edcf9dacc4564b3e8e8000abae4
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-08-19 15:37:15 -0700

    ES6 - queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,47 @@
+/* ES6 instantiation: declare classes with the keyword class
+
+Do:
+Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+Use the keyword new when instantiating your class
+Use the keyword this in your constructor
+Explicitly declare a class method named constructor
+Declare all other class methods within the class declaration
+Don't:
+Declare the instance explicitly
+Return the instance explicitly
+Add class methods to the class prototype directly
+*/
+
+// var Queue = function() {
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create Queue constructor -- this.
   constructor() {
+    this._storage = {};
+    this._start = 0;
+    this._end = 0;
   }
 
-}
+  // Use the keyword 'this' in your methods
+  // Queue.prototype.enqueue = function(value) {
+  enqueue(value) {
+    this._storage[this._end++] = value;
+  };
+
+  dequeue() {
+    // if (queueMethods.size() > 0) {
+    if (this.size() > 0) {
+      let firstElement = this._storage[this._start]
+      delete this._storage[this._start];
+      this._start++;
+      return firstElement;
+    }
+  };
+
+  size() {
+    return this._end - this._start;
+  };
+
+  // return someInstance;
+};
+
+
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,57 @@
+/* Pseudoclassical instantiation: create instances with the keyword new
+
+Do:
+Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+Use the keyword new when instantiating your class
+Use the keyword this in your constructor
+Don't:
+Declare the instance explicitly
+Return the instance explicitly
+*/
+
+/* Queue
+// A queue works like a line for an amusement park ride -- people enter at the end of the queue and leave from the front
+//(FIFO: first in, first out).
+
+enqueue(string) - Add a string to the back of the queue
+dequeue() - Remove and return the string at the front of the queue
+size() - Return the number of items in the queue
+*/
+
+// Pseudoclassical: create instances with the keyword new & the keyword this in your constructor
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // generate object
+    // var someInstance = {};
+    // var someInstance = Object.create(queueMethods);
+
+  // Create Queue constructor -- this.
+  this._storage = {};
+  this._start = 0;
+  this._end = 0;
+
+  // return someInstance;
+};
+
+//Create an object that holds the methods that will be shared by all instances of the class
+  //var queueMethods = {};
+
+// Use the keyword 'this' in your methods
+Queue.prototype.enqueue = function(value) {
+  this._storage[this._end++] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  // if (queueMethods.size() > 0) {
+  if (this.size() > 0) {
+    let firstElement = this._storage[this._start]
+    delete this._storage[this._start];
+    this._start++;
+    return firstElement;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this._end - this._start;
 };
 
 
