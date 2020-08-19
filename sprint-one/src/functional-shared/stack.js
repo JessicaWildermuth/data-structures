f630dfc60d83c04a473e279b43a270139453a82a
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-19 15:25:37 -0700

    Add methods to the stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,10 +3,27 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var stack = {};
   var storage = {};
-  var size = 0;
-  return stack;
+  return _.extend(stack, stackMethods);
 };
 
-var stackMethods = {};
+stackMethods = {};
 
 
+stackMethods.size = function() {
+  return size;
+};
+
+stackMethods.pop = function() {
+  var result;
+  if (size > 0) {
+    var result = storage[size - 1];
+    delete storage[size - 1];
+    size--;
+  }
+  return result;
+};
+
+stackMethods.push = function() {
+  storage[size] = value;
+  size++;
+};
