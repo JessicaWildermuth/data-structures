22e77f9955830cc8ebac11d05b1cf53cef7d3939
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-19 12:25:29 -0700

    Psuecode and working tests - 1

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,32 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  //create a size count to begin with,
+  //new instances shouldn't have any elements yet? can they though? ahhhh!
+  var size = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    var result;
+    if (size > 0) {
+      var result = storage[size];
+      delete storage[size];
+      size--;
+    }
+    return result;
   };
 
+
+  //need to keep size variable for all methods to access..... create global variable?
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
 };
+
+//when called return {{}}
