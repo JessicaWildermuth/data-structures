ff89d6157d3beae4db295d0f201548d3e82a7358
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-19 14:48:04 -0700

    Complete pseudocode and tests all pass

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,29 +1,33 @@
 var Queue = function() {
   var someInstance = {};
-  //size varaible to keep track of the size of the queue for all property methods to access
+  //   //size varaible to keep track of the size of the queue for all property methods to access
   var size = 0;
-  // Use an object with numeric keys to store values
+  //   // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
+  //   // Implement the methods below
 
   someInstance.enqueue = function(value) {
     //the queue is add to the back
-    //need to take the size of the queue and then add this value to the next indes (in this case the next avaialbe index would be the length as indexes are zero based);
+    //need to take the size of the queue and then add the value to the next indes (if the next avaialbe index would be the length as indexes are zero based);
     //when we add to the queue, the size increases, so we need to increment our size count by 1
     storage[size] = value;
     size++;
   };
 
   someInstance.dequeue = function() {
-    var result = storage[0];
-    delete storage[0];
-    size--;
+    //need to take the object from the 1st key value
+    var keys = Object.keys(storage);
+    keys = keys.sort();
+    var firstElement = keys.shift();
+    var result = storage[firstElement];
+    delete storage[firstElement];
     return result;
   };
 
   someInstance.size = function() {
-    return size;
+    var storageLength = Object.keys(storage).length;
+    return storageLength;
   };
 
   return someInstance;
