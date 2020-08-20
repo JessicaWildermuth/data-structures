228113c90eab7c21fab9a7f75e5204b455466197
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-19 17:49:54 -0700

    Complete Code

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeCount = 0;
 };
 
+Stack.prototype.pop = function(value) {
+  var result;
+  if (this.sizeCount > 0) {
+    result = this.storage[this.sizeCount - 1];
+    delete this.storage[this.sizeCount - 1];
+    this.sizeCount--;
+  }
+  return result;
+};
 
+Stack.prototype.push = function(value) {
+  this.storage[this.sizeCount] = value;
+  this.sizeCount++;
+};
+
+Stack.prototype.size = function() {
+  return this.sizeCount;
+};
