28427f00b6361c9de747769bc062daa342e460c2
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-19 17:34:56 -0700

    Code complete, tests pass, add methods for new instances

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.storage = {};
+  stack.sizeCount = 0;
+  return stack;
 };
 
 var stackMethods = {};
 
+stackMethods.pop = function(value) {
+  var result;
+  if (this.sizeCount > 0) {
+    var result = this.storage[this.sizeCount - 1];
+    delete this.storage[this.sizeCount - 1];
+    this.sizeCount--;
+  }
+  return result;
+};
 
+stackMethods.push = function(value) {
+  this.storage[this.sizeCount] = value;
+  this.sizeCount++;
+};
+
+stackMethods.size = function() {
+  return this.sizeCount;
+};
